f03845fc16a9e1495233ded2c7a439f4aaf16e94
wernerholzapfel <werner.holzapfel@trivento.nl>
2021-04-29 22:24:40 +0200

    nu met invalidate-cache.interceptor.ts

---
+++ src/invalidate-cache.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+    CACHE_MANAGER,
+    CallHandler,
+    ExecutionContext,
+    Inject,
+    Injectable,
+    Logger,
+    NestInterceptor
+} from '@nestjs/common';
+import {Observable} from 'rxjs';
+import {tap} from "rxjs/operators";
+import {Cache} from 'cache-manager'
+
+@Injectable()
+export class InvalidateCacheInterceptor implements NestInterceptor {
+    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
+    }
+    private readonly logger = new Logger('InvalidateCacheInterceptor', true);
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        this.logger.log(context.switchToHttp().getRequest().method)
+        return next.handle().pipe(tap(async () => {
+            if (context.switchToHttp().getRequest().method === 'POST') {
+                this.logger.log('invalidate cache')
+                await this.cacheManager.reset();
+            }
+        }));
+    }
+}
