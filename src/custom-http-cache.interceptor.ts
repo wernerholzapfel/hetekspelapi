08ded9fe5bb3e345654d0aa4aa5bf56ad92f22c5
Werner Holzapfel <werner.holzapfel@gmail.com>
2022-10-19 13:29:23 +0200

    match-prediction niet cachen

--- src/custom-http-cache.interceptor.ts
+++ src/custom-http-cache.interceptor.ts
@@ -1,13 +1,15 @@
-import {CacheInterceptor, ExecutionContext, Injectable} from "@nestjs/common";
+import { CacheInterceptor, ExecutionContext, Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export default class CustomHttpCacheInterceptor extends CacheInterceptor {
+  private readonly logger = new Logger('CustomHttpCacheInterceptor', { timestamp: true });
+
   httpServer: any;
   trackBy(context: ExecutionContext): string | undefined {
     const request = context.switchToHttp().getRequest();
     const isGetRequest = request.method === 'GET';
     const requestURl = request.path;
-    const excludePaths = ['/match-prediction/today','/participant/mine'];
+    const excludePaths = ['/match-prediction/today', '/participant/mine', '/match-prediction'];
 
     if (
       !isGetRequest ||
