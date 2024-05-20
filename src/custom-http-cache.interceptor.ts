6a650a54919e43883e0d6a3890298a085dbbf186
Werner Holzapfel <werner.holzapfel@gmail.com>
2024-05-20 20:56:03 +0200

    cache afgehaald van stats/participants

--- src/custom-http-cache.interceptor.ts
+++ src/custom-http-cache.interceptor.ts
@@ -9,7 +9,7 @@ export default class CustomHttpCacheInterceptor extends CacheInterceptor {
     const request = context.switchToHttp().getRequest();
     const isGetRequest = request.method === 'GET';
     const requestURl = request.path;
-    const excludePaths = ['/match-prediction/today', '/participant/mine', '/match-prediction', '/poule-prediction', '/knockout/mine'
+    const excludePaths = ['/match-prediction/today', '/stats/participants', '/participant/mine', '/match-prediction', '/poule-prediction', '/knockout/mine'
     , 'poule-prediction/poule/a', 'poule-prediction/poule/b', 'poule-prediction/poule/c', 'poule-prediction/poule/d'
     , 'poule-prediction/poule/e', 'poule-prediction/poule/f', 'poule-prediction/poule/g', 'poule-prediction/poule/h'];
 
