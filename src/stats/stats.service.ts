7f580a89455279bd6bb6b64a2a24176331666cc0
Werner Holzapfel <werner.holzapfel@gmail.com>
2024-05-21 11:37:38 +0200

    troostfinale eruit gehaald.

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -131,15 +131,17 @@ export class StatsService {
                         count: this.getNumberOfPredictedForRound(knockoutPredictions, team, '4')
                     }
                 }).sort((a, b) => b.count - a.count)
-            }, {
-                round: '3',
-                teams: teams.map(team => {
-                    return {
-                        ...team,
-                        count: this.getNumberOfSelectedForRound(knockoutPredictions, team, '3')
-                    }
-                }).sort((a, b) => b.count - a.count)
-            }, {
+            }
+            // , {
+            //     round: '3',
+            //     teams: teams.map(team => {
+            //         return {
+            //             ...team,
+            //             count: this.getNumberOfSelectedForRound(knockoutPredictions, team, '3')
+            //         }
+            //     }).sort((a, b) => b.count - a.count)
+            // }
+            , {
                 round: '2',
                 teams: teams.map(team => {
                     return {
